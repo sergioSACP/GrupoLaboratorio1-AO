6105baf117e3304a43c746a7514f9a75500b2a93
sergioSACP <00016126@uca.edu.sv>
2026-09-10 14:44:40 -0600

    feat: agregar funciones para gestionar calorías
    Se agregaron las funciones QuemarCalorias y AgregarCalorias para permitir el registro de calorías quemadas y agregadas.

--- main.cc
+++ main.cc
@@ -1,7 +1,37 @@
 #include <iostream>
 
-int main(){
+
+struct Usuario {
+    std::string nombre;
+    int edad;
+    float calorias;
+};
+
+
+int AgregarCalorias(float &calorias, float x) {
+    // Aquí puedes implementar la lógica para agregar calorías
+
+}
+
+int QuemarCalorias(float &calorias, float x) {
+    // Aquí puedes implementar la lógica para agregar calorías
+
+}
+
+
+int main() {
+    
 
 
     return 0;
+}
+
+int AgregarCalorias(float &calorias, float x) {
+    calorias += 200;
+    return 0;
+}
+
+int QuemarCalorias(float &calorias, float x) {
+    // Aquí puedes implementar la lógica para agregar calorías
+
 }
